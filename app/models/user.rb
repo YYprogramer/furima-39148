59bda43602607b11565261c963f7babe319e87b6
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-07-27 05:47:40 +0900

    ユーザー情報の登録
    パスワード設定時に６文字以上半角英数字のバリデーションを設定しています

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
 end
