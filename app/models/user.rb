25b1eb1b306f66890aee12015d8ebe42f9219e87
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-07-30 06:18:25 +0900

    メールアドレスの入力
    メールアドレスは空欄では保存できない。
    英字数字特殊記号＋＠＋英字数字特殊記号の文字列でなければ保存できない

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   validates :password, presence: true, length: { minimum: 6 }, format: { with:  /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z0-9]+\z/ },confirmation: true
   validates :password_confirmation, presence: true
+  validates :email, presence: true,format: { with: /\A[\w+\-.]+@[\w+\-.]\z/ }
 end
