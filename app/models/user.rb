8726f928d01b720d5bb1c54f6e38720e77c5899d
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-07-30 06:28:47 +0900

    名前入力欄（漢字）
    漢字・ひらがな・カタカナの入力のみ許可

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }, format: { with:  /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z0-9]+\z/ },confirmation: true
   validates :password_confirmation, presence: true
   validates :email, presence: true,format: { with: /\A[\w+\-.]+@[\w+\-.]\z/ }
-  validates :family_name_kanji, presence: true
-  validates :given_name_kanji, presence: true
+  validates :family_name_kanji, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/ }
+  validates :given_name_kanji, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/ }
   validates :family_name_kana, presence: true
   validates :given_name_kana, presence: true
   validates :birthday, presence: true
