53d6f638e21d5a99f1424aa3122376b9b8d0ccb4
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-07-31 04:58:47 +0900

    未ログインユーザーのアクション修正
    未ログインユーザーでも新規登録・ログインにはアクセスできるように修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
-    sign_up: 'signup',
+    sign_up: 'signup'
   }
   root to: 'furimas#index'
 end
