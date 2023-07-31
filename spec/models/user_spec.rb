40c483decb43f4723dbeda5f06d1a48cea4f1692
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-01 05:45:57 +0900

    ファクトリーボットの導入と単体テストコードの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
   describe 'ユーザー新規登録' do
     it 'nicknameが空では保存できない' do
       user = User.new(nickname: '', email: 'test@example.com', password: 'abc000', password_confirmation: 'abc000', family_name_kanji: '実験', given_name_kanji: '実験', family_name_kana: 'ジッケン', given_name_kana: 'ジッケン', birthday:'1930-01-01')
@@ -11,6 +14,17 @@ RSpec.describe User, type: :model do
       user = User.new(nickname: 'test', email: '', password: 'abc000', password_confirmation: 'abc000', family_name_kanji: '実験', given_name_kanji: '実験', family_name_kana: 'ジッケン', given_name_kana: 'ジッケン', birthday:'1930-01-01')
       user.valid?
       expect(user.errors.full_messages).to include("Email can't be blank")
+    end
+    it 'passwordが空では保存できない' do
+      user = User.new(nickname: 'test', email: 'test@example.com', password: '', password_confirmation: 'abc000', family_name_kanji: '実験', given_name_kanji: '実験', family_name_kana: 'ジッケン', given_name_kana: 'ジッケン', birthday:'1930-01-01')
+      user.valid?
+      expect(user.errors.full_messages).to include("Password can't be blank")
+    end
+    it 'passwordとpassword_confirmationが不一致だと登録できない' do
+      @user.password = 'abc123'
+      @user.password_confirmation = 'abc124'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
   end
 end
