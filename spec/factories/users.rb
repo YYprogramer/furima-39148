67fbee3f675523e22b6b1487a1a2457660654a79
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-02 04:27:07 +0900

    ファクトリーボットの生成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname {'ニックネーム'}
+    email {'test@test.com'}
+    password {'abc123'}
+    password_confirmation {'abc123'}
+    family_name_kanji {'亜'}
+    given_name_kanji {'亜'}
+    family_name_kana {'ア'}
+    given_name_kana {'ア'}
+    birthday {'2000-01-01'}
   end
 end
