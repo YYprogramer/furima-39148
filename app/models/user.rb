dcb3b1c0911d374450154b542ebc60bee9edd4bd
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-03 04:34:34 +0900

    バリデーションの削除
    email及びpassword_confirmationはdeviceのデフォルト機能でバリデーションが設定されているため、削除しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,6 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)/ },confirmation: true
-  validates :password_confirmation, presence: true
-  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :family_name_kanji, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/ }
   validates :given_name_kanji, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/ }
   validates :family_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
