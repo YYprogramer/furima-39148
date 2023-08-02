79aecb56d5fe6446afed99103fdefb8279ae1bf6
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-03 05:32:43 +0900

    テストコードの修正
    不要な空白を削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,18 +45,18 @@ RSpec.describe User, type: :model do
       it 'passwordが空では保存できない' do
         @user.password = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password can't be  blank")
+        expect(@user.errors.full_messages).to include("Password can't be blank")
       end
       it 'passwordとpassword_confirmationが不一致だと登録できない' do
         @user.password = 'abc123'
         @user.password_confirmation = 'abc124'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation  doesn't match Password")
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       it 'passwordが5文字以下だと保存できない' do
         @user.password = 'abc12'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short  (minimum is 6 characters)")
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
       it 'passwordが英語のみだと保存できない' do
         @user.password = 'abcdef'
@@ -73,7 +73,7 @@ RSpec.describe User, type: :model do
         another_user = FactoryBot.build(:user)
         another_user.email = @user.email
         another_user.valid?
-        expect(another_user.errors.full_messages).to include('Email has   already been taken')
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
       it 'emailに@が含まれていないと保存できない' do
         @user.email = 'abc.abc'
@@ -83,12 +83,12 @@ RSpec.describe User, type: :model do
       it 'family_name_kanjiが英語だと保存できない' do
         @user.family_name_kanji = 'abc'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family name kanji is   invalid")
+        expect(@user.errors.full_messages).to include("Family name kanji is invalid")
       end
       it 'given_name_kanjiが英語だと保存できない' do
         @user.given_name_kanji = 'abc'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Given name kanji is  invalid")
+        expect(@user.errors.full_messages).to include("Given name kanji is invalid")
       end
       it 'family_name_kanaが漢字だと保存できない' do
         @user.family_name_kana = '亜'
