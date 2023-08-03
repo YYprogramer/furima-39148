4d6abf276deab8a0840fd2b77ba9b15786df8932
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-03 19:11:51 +0900

    ユーザーコントローラーの修正
    destroyアクションはまだ実装しないためコメントアウト

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -3,8 +3,8 @@ class FurimasController < ApplicationController
     render 'items/index'
   end
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_path
-  end
+  # def destroy
+  #   session[:user_id] = nil
+  #   redirect_to root_path
+  # end
 end
