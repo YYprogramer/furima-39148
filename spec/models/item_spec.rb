a08d25a7efcf88493a8ff612a9337dfd92a19c0a
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-12 05:45:18 +0900

    テストコードの修正
    冗長な記述の簡略化を行なった

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,69 +8,58 @@ RSpec.describe Item, type: :model do
   describe '商品出品機能' do
     context '新規登録がうまくいく場合' do
       it 'name,description,condition_id,send_cost_id,send_origin_id,send_time_id,category_id,price,user_id,imageがあれば保存できる' do
-        @user = create(:user) 
         @item = build(:item, user: @user) 
         expect(@item).to be_valid
       end
     end
     context '新規登録がうまくいかない場合' do
       it 'nameが空だと保存できない' do
-      @user = create(:user)
-      @item = build(:item, user: @user, name: "")
+      @item.name = ""
       @item.valid?
       expect(@item.errors.full_messages).to include("Name can't be blank")
       end
       it 'descriptionが空だと保存できない'do
-      @user = create(:user)
-      @item = build(:item, user: @user, description: "")
+      @item.description = ""
       @item.valid?
       expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it 'category_idが1だと保存できない'do
-      @user = create(:user)
-      @item = build(:item, user: @user, category_id: "1")
+      @item.category_id = "1"
       @item.valid?
       expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it 'condition_idが1だと保存できない'do
-      @user = create(:user)
-      @item = build(:item, user: @user, condition_id: "1")
+      @item.condition_id = "1"
       @item.valid?
       expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it 'send_cost_idが1だと保存できない'do
-      @user = create(:user)
-      @item = build(:item, user: @user, send_cost_id: "1")
+      @item.send_cost_id = "1"
       @item.valid?
       expect(@item.errors.full_messages).to include("Send cost can't be blank")
       end
       it 'send_origin_idが1だと保存できない'do
-      @user = create(:user)
-      @item = build(:item, user: @user, send_origin_id: "1")
+      @item.send_origin_id = "1"
       @item.valid?
       expect(@item.errors.full_messages).to include("Send origin can't be blank")
       end
       it 'send_time_idが1だと保存できない'do
-      @user = create(:user)
-      @item = build(:item, user: @user, send_time_id: "1")
+      @item.send_time_id = "1"
       @item.valid?
       expect(@item.errors.full_messages).to include("Send time can't be blank")
       end
       it 'priceが空だと保存できない'do
-      @user = create(:user)
-      @item = build(:item, user: @user, price: "")
+      @item.price = ""
       @item.valid?
       expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it 'priceが299円以下だと保存できない'do
-      @user = create(:user)
-      @item = build(:item, user: @user, price: "299")
+      @item.price = "299"
       @item.valid?
       expect(@item.errors.full_messages).to include("Price is invalid")
       end
       it 'priceが10,000,000円以上だと保存できない'do
-      @user = create(:user)
-      @item = build(:item, user: @user, price: "10000000")
+      @item.price = "10000000"
       @item.valid?
       expect(@item.errors.full_messages).to include("Price is invalid")
       end
