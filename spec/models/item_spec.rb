03e8d2ac64853b1d12a096922441ce4450f909c1
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-12 06:05:43 +0900

    テストコードの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -63,6 +63,21 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Price is invalid")
       end
+      it 'priceが半角英数字でなければ保存できない'do
+      @item.price = "A１"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+      it 'imageが空だと保存できない'do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'userが結びついていないと保存できない'do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("User must exist")
+      end
     end
   end 
 end
