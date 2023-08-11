be2323ae5cbb507be9a11485c7cdb9e060e7fd89
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-11 15:09:52 +0900

    非同期通信による販売手数料と販売利益の計算

---
+++ app/javascript/packs/calc.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const priceInput = document.getElementById('item-price');
+  const addTaxPrice = document.getElementById('add-tax-price');
+  const profit = document.getElementById('profit');
+
+  priceInput.addEventListener('input', function() {
+    const price = parseFloat(priceInput.value);
+    if (!isNaN(price)) {
+      const tax = price * 0.1;
+      const profitAmount = price - tax;
+
+      addTaxPrice.textContent = tax.toFixed(0);
+      profit.textContent = profitAmount.toFixed(0);
+    } else {
+      addTaxPrice.textContent = '';
+      profit.textContent = '';
+    }
+  });
+});
