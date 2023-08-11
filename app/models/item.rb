dab3a028b5599ecc1806dd2bb77578755e4eae69
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-11 12:55:25 +0900

    アクティブハッシュの実装
    商品状態
    発送料の負担
    発送元
    発送までの日数
    にアクティブハッシュの実装を行う

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,13 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
+  belongs_to :send_cost
+  belongs_to :send_origin
+  belongs_to :send_time
   belongs_to :user
   has_one_attached :image
 
   validates :image, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, :condition_id, :send_cost_id, :send_origin_id, :send_time_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
