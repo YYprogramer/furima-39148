4245e750002a962ec2974947ef20de64e9dcd297
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-13 14:46:21 +0900

    furimas_controllerの復元

---
+++ app/controllers/furimas_controller.rb
@@ -0,0 +1,10 @@
+class FurimasController < ApplicationController
+  def index
+    render 'items/index'
+  end
+
+  # def destroy
+  #   session[:user_id] = nil
+  #   redirect_to root_path
+  # end
+end
