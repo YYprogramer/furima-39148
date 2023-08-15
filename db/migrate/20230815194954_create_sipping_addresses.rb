86b4c161b00bb55efbf09b50b6f818552ab98b4c
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-16 05:10:34 +0900

    SippingAddressのマイグレーションファイル作成

--- db/migrate/20230815194954_create_sipping_addresses.rb
+++ db/migrate/20230815194954_create_sipping_addresses.rb
@@ -1,6 +1,13 @@
 class CreateSippingAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :sipping_addresses do |t|
+      t.string     :postal_code,    null: false
+      t.integer    :send_origin_id, null: false
+      t.string     :city,           null: false
+      t.string     :street_address, null: false
+      t.string     :building        
+      t.string     :phone_number,   null: false
+      t.references :item,           null: false, foreign_key: true
 
       t.timestamps
     end
