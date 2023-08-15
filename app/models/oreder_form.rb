68b0921c914aa3028de997e058c15c9f088b2b48
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-16 04:58:55 +0900

    order_formの作成

---
+++ app/models/oreder_form.rb
@@ -0,0 +1,11 @@
+class クラス名
+  include ActiveModel::Model
+  attr_accessor  :user_id, :item_id,
+
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+
+  def save
+    # 各テーブルにデータを保存する処理を書く
+  end
+end
