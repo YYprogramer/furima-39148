3a0452c0d93cefbdce9cc691cfb5a536a8e7bc86
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-15 14:16:35 +0900

    コードレビューの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :find_item, only: [:show, :edit, :update]
+
 
   def index
     @items = Item.order(created_at: :desc)
@@ -19,18 +21,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    @item
   end
 
   def edit
-    @item = Item.find(params[:id])
-    if user_signed_in? && @item.user != current_user
+    @item
+    if @item.user != current_user
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to @item
     else
@@ -42,5 +43,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :category_id, :condition_id, :send_cost_id, :send_origin_id, :send_time_id, :price, :name, :description)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 end
