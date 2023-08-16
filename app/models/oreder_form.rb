65a9c7216e9e333bad2f9b4656d61d2a8d0131d4
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-16 18:54:25 +0900

    OrderFormModelの実装

--- app/models/oreder_form.rb
+++ app/models/oreder_form.rb
@@ -1,11 +1,20 @@
-class クラス名
+class OrderForm
   include ActiveModel::Model
-  attr_accessor  :user_id, :item_id
+  attr_accessor  :user_id, :item_id, :send_origin_id
 
   validates :user_id, presence: true
   validates :item_id, presence: true
+  validates :send_origin_id, presence: true
+
 
   def save
-    # 各テーブルにデータを保存する処理を書く
+    if valid?
+      order = Order.create(user_id: user_id, item_id: item_id)
+
+      shipping_address = ShippingAddress.create(order_id: order.id, send_origin_id: send_origin_id)
+      return true
+    else
+      return false
+    end
   end
 end
