b00e189e3d210eb31d988b84f5c35801e464eba4
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-18 05:11:54 +0900

    PAYJPの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,15 +8,27 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
-      redirect_to item_path(@item)
+      pay_item
+      @order_form.save
+      return redirect_to root_path
     else
       render :index
     end
   end
-
+  
   private
-
+  
   def order_params
     params.require(:order_form).permit(:user_id, :item_id)
+    params.require(:order).permit(:price).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :shipping_address
+  attr_accessor :token
 
-  validates :user, :item, :shipping_address , presence: true
+  validates :user, :item, :shipping_address ,:token, presence: true
 end
