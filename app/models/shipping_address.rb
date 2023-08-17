2b2341e47cc8b079b53811275c6b781daebd87b5
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-18 03:49:00 +0900

    郵便番号のバリデーション
    3桁の数字-4桁の数字
    となっていないと保存できない

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -4,4 +4,5 @@ class ShippingAddress < ApplicationRecord
 
   validates :order,presence: true
   validates :send_origin_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "Please enter in the format of '3-digit hyphen 4-digit'" }
 end
