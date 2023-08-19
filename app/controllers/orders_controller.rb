0904c14e9b49b563fd3afbec40446a1b69724543
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-20 04:28:56 +0900

    商品購入機能の実装
    各モデルに保存できるように修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,10 +7,14 @@ class OrdersController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(order_params)
+    @order_form.user_id = current_user.id
+    @order_form.item_id = @item.id
+    @order_form.token = params[:token]
+
     if @order_form.valid?
       pay_item
-      @order_form.save
-      return redirect_to root_path
+      order = @order_form.save(user_id: current_user.id, item_id: @item.id)
+      redirect_to root_path
     else
       render :index
     end
@@ -19,15 +23,14 @@ class OrdersController < ApplicationController
   private
   
   def order_params
-    params.require(:order_form).permit(:user_id, :item_id)
-    params.require(:order).permit(:price).merge(token: params[:token])
+    params.require(:order_form).permit(:card_number, :card_exp_month, :card_exp_year, :card_cvc,:postal_code, :send_origin_id, :city, :street_address,:building, :phone_number, :user_id, :item_id, :token, :price)
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],
-      card: order_params[:token],
+      amount: @item.price,
+      card: @order_form.token,
       currency: 'jpy'
     )
   end
