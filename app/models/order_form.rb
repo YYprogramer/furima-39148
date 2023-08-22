3b4796fec4eae1cb39c24e983e996aa41dd6c3dc
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-23 04:11:25 +0900

    バリデーションの記述場所変更

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -2,11 +2,11 @@ class OrderForm
   include ActiveModel::Model
   attr_accessor :postal_code, :send_origin_id , :city, :street_address, :building, :phone_number, :user_id, :item_id, :token
 
-  validates :postal_code, presence: true
-  validates :send_origin_id, presence: true
+  validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "Please enter in the format of '3-digit hyphen 4-digit'" }, presence: true
+  validates :send_origin_id, numericality: { other_than: 1 , message: "can't be blank"}, presence: true
   validates :city, presence: true
   validates :street_address, presence: true
-  validates :phone_number, presence: true
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "Please enter a 10 to 11 digit numeric value." }, presence: true
   validates :user_id, presence: true
   validates :item_id, presence: true
   validates :token, presence: true
@@ -14,8 +14,8 @@ class OrderForm
 
   def save(user_id:, item_id:)
     if valid?
-      order = Order.create(user_id: user_id, item_id: item_id)
-      shipping_address = ShippingAddress.create(postal_code: postal_code, send_origin_id: send_origin_id, city: city, street_address: street_address,phone_number: phone_number)
+        order = Order.create(user_id: user_id, item_id: item_id)
+        shipping_address = ShippingAddress.create(postal_code: postal_code, send_origin_id: send_origin_id, city: city, street_address: street_address, building: building, phone_number: phone_number)
       return true
     else
       return false
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -2,8 +2,5 @@ class ShippingAddress < ApplicationRecord
   belongs_to :order
   belongs_to :send_origin
 
-  validates :order,presence: true
-  validates :send_origin_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "Please enter in the format of '3-digit hyphen 4-digit'" }
-  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "Please enter a 10 to 11 digit numeric value." }
+  validates :order, :postal_code,:send_origin_id, :city, :street_address, :phone_number, presence: true
 end
