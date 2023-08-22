1733388e83ee83299e3467d76af05ac4f3df21f6
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-23 04:52:15 +0900

    バリデーションの削除
    Formオブジェクト内でバリデーションを設定しているため、各モデル内でのバリデーションは削除しています

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -2,5 +2,4 @@ class ShippingAddress < ApplicationRecord
   belongs_to :order
   belongs_to :send_origin
 
-  validates :order, :postal_code,:send_origin_id, :city, :street_address, :phone_number, presence: true
 end
