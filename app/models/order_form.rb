02b264fb0c0c7632a9a3056bf341795a0a15f3e5
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-27 19:40:29 +0900

    with＿optionsを使用しコードの可読性を高める

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -2,14 +2,16 @@ class OrderForm
   include ActiveModel::Model
   attr_accessor :postal_code, :send_origin_id , :city, :street_address, :building, :phone_number, :user_id, :item_id, :token
 
-  validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "Please enter in the format of '3-digit hyphen 4-digit'" }, presence: true
-  validates :send_origin_id, numericality: { other_than: 1 , message: "can't be blank"}, presence: true
-  validates :city, presence: true
-  validates :street_address, presence: true
-  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "Please enter a 10 to 11 digit numeric value." }, presence: true
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-  validates :token, presence: true
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "Please enter in the format of '3-digit hyphen 4-digit'" }
+    validates :send_origin_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :street_address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "Please enter a 10 to 11 digit numeric value." }
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
 
 
   def save
