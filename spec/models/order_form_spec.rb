7faa6b19db88068f8719af40941e4d2d3860da75
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-08-27 17:43:26 +0900

    OrderFormのテストコード実施

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -21,34 +21,62 @@ RSpec.describe OrderForm, type: :model do
     context '商品購入が失敗する場合' do
       it 'postal_codeが空の場合、保存できない' do
         @order_form.postal_code = ''
+        @order_form.valid?
         expect(@order_form).to_not be_valid
       end
       it 'postal_codeが「3文字-4文字」ではない場合、保存できない' do
         @order_form.postal_code = '1111-111'
+        @order_form.valid?
         expect(@order_form).to_not be_valid
       end
       it 'postal_codeに「-」がない場合、保存できない' do
         @order_form.postal_code = '1111111'
+        @order_form.valid?
+        expect(@order_form).to_not be_valid
+      end
+      it 'postal_codeに全角数字が含まれている場合、保存できない' do
+        @order_form.postal_code = '１11-1111'
+        @order_form.valid?
         expect(@order_form).to_not be_valid
       end
       it 'send_origin_idの値が１の場合、保存できない' do
         @order_form.send_origin_id = '1'
+        @order_form.valid?
         expect(@order_form).to_not be_valid
       end
       it 'cityが空の場合、保存できない' do
         @order_form.city = ''
+        @order_form.valid?
         expect(@order_form).to_not be_valid
       end
       it 'street_addressが空の場合、保存できない' do
         @order_form.street_address = ''
+        @order_form.valid?
         expect(@order_form).to_not be_valid
       end
       it 'phone_numberが空の場合、保存できない' do
         @order_form.phone_number = ''
+        @order_form.valid?
+        expect(@order_form).to_not be_valid
+      end
+      it 'phone_numberが10桁以下の場合、保存できない' do
+        @order_form.phone_number = '090123456'
+        @order_form.valid?
+        expect(@order_form).to_not be_valid
+      end
+      it 'phone_numberが11桁以上の場合、保存できない' do
+        @order_form.phone_number = '090123456789'
+        @order_form.valid?
+        expect(@order_form).to_not be_valid
+      end
+      it 'phone_numberに全角数字が含まれていた場合、保存できない' do
+        @order_form.phone_number = '０9012345678'
+        @order_form.valid?
         expect(@order_form).to_not be_valid
       end
       it 'tokenが空の場合、保存できない' do
         @order_form.token = ''
+        @order_form.valid?
         expect(@order_form).to_not be_valid
       end
     end
